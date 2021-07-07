42276f44f53d802ccf7eb2d47b8c52765319b124
Jaime <67835355+jaimemodestojr@users.noreply.github.com>
2021-07-07 13:05:43 -0300

    Colocando o namespace
    Havia esquecido de colocar o namespace no código, agora foi adicionado.

--- Program.cs
+++ Program.cs
@@ -1,32 +1,35 @@
 ﻿using System;
 
-public class Program
+namespace sorteioGrupoVersao1
 {
-	public static void Main()
+	public class Program
 	{
-		string[] alunos = new string[] { "Aluno1","Aluno2", "Aluno3", "Aluno4", "Aluno5", "Aluno6", "Aluno7", "Aluno8",
-											"Aluno9", "Aluno10", "Aluno11", "Aluno12", "Aluno13", "Aluno14", "Aluno15",
-												"Aluno16", "Aluno17", "Aluno18", "Aluno19", "Aluno20" };
-
-		Random sorteio = new Random();
-		for (int i = 0; i < alunos.Length - 1; i++)
+		public static void Main()
 		{
-			int j = sorteio.Next(i, alunos.Length);
-			var temp = alunos[i];
-			alunos[i] = alunos[j];
-			alunos[j] = temp;
-		}
+			string[] alunos = new string[] { "Aluno1","Aluno2", "Aluno3", "Aluno4", "Aluno5", "Aluno6", "Aluno7", "Aluno8",
+												"Aluno9", "Aluno10", "Aluno11", "Aluno12", "Aluno13", "Aluno14", "Aluno15",
+													"Aluno16", "Aluno17", "Aluno18", "Aluno19", "Aluno20" };
 
-		int y = 0, grupo = 1;
-		foreach (string x in alunos)
-		{
-			if (y % 4 == 0)
+			Random sorteio = new Random();
+			for (int i = 0; i < alunos.Length - 1; i++)
 			{
-				Console.WriteLine("\n\nGrupo " + grupo + ":");
-				grupo++;
+				int j = sorteio.Next(i, alunos.Length);
+				var temp = alunos[i];
+				alunos[i] = alunos[j];
+				alunos[j] = temp;
 			}
-			Console.WriteLine(" > " + x);
-			y++;
+
+			int y = 0, grupo = 1;
+			foreach (string x in alunos)
+			{
+				if (y % 4 == 0)
+				{
+					Console.WriteLine("\n\nGrupo " + grupo + ":");
+					grupo++;
+				}
+				Console.WriteLine(" > " + x);
+				y++;
+			}
 		}
 	}
 }
